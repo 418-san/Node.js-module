59a09dc940ca823479d9fb6625a9c293f99bbb06
418-san <fen1cs95@gmail.com>
2019-11-14 15:17:09 +0300

    first hw

---
+++ JS_Homewors/Homework_1/js/1.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// 3_task
+function popBox () 
+{
+    x = confirm(Math.min(9, 25, 13)); 
+
+    if (x == true )
+    {
+        document.write('Вы нажали ок');
+    }
+}
+
+// 4_ task
+let x = 10;
+let m = 9;
+let n = 20;
+
+console.log(m < n);
+console.log(x > m);
+
+console.log(m);
+
